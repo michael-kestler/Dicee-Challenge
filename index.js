1525defc35098d936b14ad424ba2f7659d37c06e
michael-kestler <michael.s.kestler@gmail.com>
2022-10-13 21:31:30 -0700

    changed diceroll function and added result for second die when clicked

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 
 
 
-
 function diceRoll(){
-  let randomNumber1 =  Math.floor(Math.random() * 6) + 1;
-  if(randomNumber1 == 1) img1.setAttribute('src', 'images/dice1.png')
-  else if(randomNumber1 == 2) img1.setAttribute('src', 'images/dice2.png')
-  else if(randomNumber1 == 3) img1.setAttribute('src', 'images/dice3.png')
-  else if(randomNumber1 == 4) img1.setAttribute('src', 'images/dice4.png')
-  else if(randomNumber1 == 5) img1.setAttribute('src', 'images/dice5.png')
-  else if(randomNumber1 == 6) img1.setAttribute('src', 'images/dice6.png')
+  var randomNumber1 =  Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 =  Math.floor(Math.random() * 6) + 1;
+  document.querySelector('.img1').setAttribute('src', 'images/dice' + randomNumber1 + '.png');
+  document.querySelector('.img2').setAttribute('src', 'images/dice' + randomNumber2 + '.png');
 
 }
+
+
+// function rollTheDice() {
+//         setTimeout(function () {
+//             var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+//             var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+//
+//             document.querySelector(".img1").setAttribute("src",
+//                 "dice" + randomNumber1 + ".png");
+//
+//             document.querySelector(".img2").setAttribute("src",
+//                 "dice" + randomNumber2 + ".png");
+//
+//             if (randomNumber1 === randomNumber2) {
+//                 document.querySelector("h1").innerHTML = "Draw!";
+//             }
+//
+//             else if (randomNumber1 < randomNumber2) {
+//                 document.querySelector("h1").innerHTML
+//                                 = (player2 + " WINS!");
+//             }
+//
+//             else {
+//                 document.querySelector("h1").innerHTML
+//                                 = (player1 + " WINS!");
+//             }
+//         }, 2500);
+//     }
