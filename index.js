b86caa4680f9a9de4ed89030da8f8774487b2531
michael-kestler <michael.s.kestler@gmail.com>
2022-10-13 22:07:00 -0700

    changed size of the h1

--- index.js
+++ index.js
@@ -15,31 +15,3 @@ function diceRoll(){
     document.querySelector('h1').innerHTML="It's a draw";
   }
 }
-
-
-// function rollTheDice() {
-//         setTimeout(function () {
-//             var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-//             var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-//
-//             document.querySelector(".img1").setAttribute("src",
-//                 "dice" + randomNumber1 + ".png");
-//
-//             document.querySelector(".img2").setAttribute("src",
-//                 "dice" + randomNumber2 + ".png");
-//
-//             if (randomNumber1 === randomNumber2) {
-//                 document.querySelector("h1").innerHTML = "Draw!";
-//             }
-//
-//             else if (randomNumber1 < randomNumber2) {
-//                 document.querySelector("h1").innerHTML
-//                                 = (player2 + " WINS!");
-//             }
-//
-//             else {
-//                 document.querySelector("h1").innerHTML
-//                                 = (player1 + " WINS!");
-//             }
-//         }, 2500);
-//     }
