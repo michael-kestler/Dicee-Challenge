4a502face4179c3c1faef7a67f727191979a9b57
michael-kestler <michael.s.kestler@gmail.com>
2022-10-13 20:18:03 -0700

    added function that changes dice picture randomly when image is clicked

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-let randomNumber1 =  Math.floor(Math.random() * 6) + 1;
+
+
+
+
+function diceRoll(){
+  let randomNumber1 =  Math.floor(Math.random() * 6) + 1;
+  if(randomNumber1 == 1) img1.setAttribute('src', 'images/dice1.png')
+  else if(randomNumber1 == 2) img1.setAttribute('src', 'images/dice2.png')
+  else if(randomNumber1 == 3) img1.setAttribute('src', 'images/dice3.png')
+  else if(randomNumber1 == 4) img1.setAttribute('src', 'images/dice4.png')
+  else if(randomNumber1 == 5) img1.setAttribute('src', 'images/dice5.png')
+  else if(randomNumber1 == 6) img1.setAttribute('src', 'images/dice6.png')
+
+}
